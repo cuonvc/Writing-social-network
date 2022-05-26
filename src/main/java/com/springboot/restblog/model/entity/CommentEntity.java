d10fe8e846ee7570371054cb9af9e26d0b83401e
Cuong Nguyen <cuongnvc5802@gmail.com>
2022-05-26 09:52:04 +0700

    Create comment entity and constraint ManyToOne to post

--- src/main/java/com/springboot/restblog/model/entity/CommentEntity.java
+++ src/main/java/com/springboot/restblog/model/entity/CommentEntity.java
@@ -5,29 +5,28 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "posts")
-public class PostEntity {
+@Table(name = "comments")
+public class CommentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
-    private String title;
+    private String name;
 
     @Column(nullable = false)
-    private String description;
+    private String email;
 
     @Column(nullable = false)
     private String content;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<CommentEntity> commentEntities = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private PostEntity post;
 }
