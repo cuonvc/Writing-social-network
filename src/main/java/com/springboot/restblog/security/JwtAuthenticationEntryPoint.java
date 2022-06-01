a471f3b5beebf87c2dd1733d280b8958e3f94315
Cuong Nguyen <cuongnvc5802@gmail.com>
2022-06-01 21:11:46 +0700

    Add JWT to spring boot app

---
+++ src/main/java/com/springboot/restblog/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.springboot.restblog.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    //method sẽ được gọi khi một exception thông qua một unauthentication user cố gắng truy cập vào vùng không được phép
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
