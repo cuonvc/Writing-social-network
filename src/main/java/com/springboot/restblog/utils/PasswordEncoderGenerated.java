79f4085bcc1cf2b057cf6b6717ace75b247465aa
cuonvc <cuong.fithau@gmail.com>
2022-08-17 19:25:55 +0700

    make it better

--- src/main/java/com/springboot/restblog/utils/PasswordEncoderGenerated.java
+++ src/main/java/com/springboot/restblog/utils/PasswordEncoderGenerated.java
@@ -6,6 +6,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class PasswordEncoderGenerated {
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        //
+        System.out.println(passwordEncoder.encode("cuongadmin"));
     }
 }
